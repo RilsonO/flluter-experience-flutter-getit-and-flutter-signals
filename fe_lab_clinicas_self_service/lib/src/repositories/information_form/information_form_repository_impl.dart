847f6e96b3e4d7fbfa20dcb8a8732e7ecc66cc16
RilsonO <rilson2.0@gmail.com>
2024-02-17 13:56:16 -0300

    first commint.

---
+++ fe_lab_clinicas_self_service/lib/src/repositories/information_form/information_form_repository_impl.dart
@@ -0,0 +1,48 @@
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+import 'package:fe_lab_clinicas_core/fe_lab_clinicas_core.dart';
+import 'package:fe_lab_clinicas_self_service_cb/src/model/patient_model.dart';
+import 'package:fe_lab_clinicas_self_service_cb/src/model/self_service_model.dart';
+
+import './information_form_repository.dart';
+
+class InformationFormRepositoryImpl implements InformationFormRepository {
+  InformationFormRepositoryImpl({
+    required this.restClient,
+  });
+
+  final RestClient restClient;
+
+  @override
+  Future<Either<RepositoryException, Unit>> register(
+      SelfServiceModel model) async {
+    try {
+      final SelfServiceModel(
+        :name!,
+        :lastName!,
+        patient: PatientModel(id: patientId)!,
+        documents: {
+          DocumentType.healthInsuranceCard: List(first: healthInsuranceCardDoc),
+          DocumentType.medicalOrder: medicalOrderDocs,
+        }!
+      ) = model;
+
+      await restClient.auth.post('/patientInformationForm', data: {
+        'patient_id': patientId,
+        'health_insurance_card': healthInsuranceCardDoc,
+        'medical_order': medicalOrderDocs,
+        'password': '$name $lastName',
+        'data_created': DateTime.now().toIso8601String(),
+        'status': 'waiting',
+        'tests': []
+      });
+
+      return Right(unit);
+    } on DioException catch (e, s) {
+      log('Erro ao finalizar formulário de auto atentimento',
+          error: e, stackTrace: s);
+      return Left(RepositoryException());
+    }
+  }
+}
